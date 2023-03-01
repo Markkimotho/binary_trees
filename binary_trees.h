18f33e3cc0391f23f316ef3a4c2163db2c171d95
Markkimotho <martinozas@gmail.com>
2023-03-01 11:16:34 +0300

    Basic binary tree Operations

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,28 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
+#ifndef _BINARY_TREES_
+#define _BINARY_TREES_
 
 
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
 
 
 /* Prototypes */
@@ -25,23 +45,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t; 
